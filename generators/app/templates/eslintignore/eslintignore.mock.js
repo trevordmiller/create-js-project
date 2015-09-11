9f2191589e8feba5eee653536ffa04736f480131
Trevor D. Miller <trevordmiller@icloud.com>
2015-09-10 19:33:59 -0600

    Fix eslintignore

--- generators/app/templates/eslintignore/eslintignore.mock.js
+++ generators/app/templates/eslintignore/eslintignore.mock.js
@@ -1,21 +1,4 @@
 export const lintingMock =
-`{
-  "stage": 0,
-  "plugins": [
-    "react-transform"
-  ],
-  "extra": {
-    "react-transform": [
-      {
-        "target": "react-transform-webpack-hmr",
-        "imports": ["react"],
-        "locals": ["module"]
-      },
-      {
-        "target": "react-transform-catch-errors",
-        "imports": ["react", "delicate-error-reporter"]
-      }
-    ]
-  }
-}
+`node_modules
+dist/
 `;
